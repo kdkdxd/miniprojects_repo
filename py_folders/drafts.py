51b0bef4f7dc389a1751f2587676d1ebed070a7a
kdkdxd <kdkdmoneyaholic@gmail.com>
2026-07-22 00:00:12 +0700

    mô tả

--- py_folders/drafts.py
+++ py_folders/drafts.py
@@ -6,40 +6,20 @@ import seaborn as sns
 import scipy.stats as stats
 import sys
 
+chi_tieu = pd.Series({
+    "Food":      2500000,
+    "Rent":      3000000,
+    "Transport": 500000,
+    "Entertain": 800000
+})
 
+print(chi_tieu)
+print(f"Money spent on Food : {chi_tieu["Food"]}")
+print(chi_tieu.iloc[3])
+print(chi_tieu.iloc[1])
 
-
-diem = np.array([7.5, 8.0, 6.5, 9.0, 7.0, 8.5, 5.5, 9.5])
-
-print(np.var(diem).round(2))
-print(np.percentile(diem,25))
-print(np.percentile(diem,50))
-print(np.percentile(diem,75))
-
-
-doanh_thu = np.array([
-    [120, 150, 200, 180],   # NV A
-    [200, 220, 190, 210],   # NV B
-    [95,  110, 130, 120],   # NV C
-])
-
-# mean each both of each workers
-print(np.mean(doanh_thu,axis=0))
-print(np.mean(doanh_thu, axis=1))
-
-#  total revenue each workers have made
-print(np.sum(doanh_thu,axis=0))
-
-# trung binh doanh thu 3 nguoi lam dc trong moi thang
-print(np.mean(doanh_thu,axis=1))
-
-
-
-
-
-
-
-
+chi_tieu["Expense"] = chi_tieu[chi_tieu>1000000]
+print(chi_tieu)
 
 
 
