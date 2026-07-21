f19bcd679e02a8a63d619a8d35d5890328b2aa81
kdkdxd <kdkdmoneyaholic@gmail.com>
2026-07-21 23:48:19 +0700

    mô tả

--- py_folders/drafts.py
+++ py_folders/drafts.py
@@ -6,32 +6,12 @@ import seaborn as sns
 import scipy.stats as stats
 import sys
 
-sys.stdout.reconfigure(encoding="utf-8")
-
-score = [ 90, 80, 70 , 65, 48, 400, 600 ] #  
-
-score.append(67)
-score.insert(3,77)
-score.remove(48)
-score.sort(reverse=True)
 
 
 
-employees = {
-    "E001": {"name": "Nguyen Van A", "dept": "Data", "salary": 15000000},
-    "E002": {"name": "Tran Thi B",  "dept": "AI",   "salary": 18000000},
-    "E003": {"name": "Le Van C",    "dept": "Data", "salary": 12000000},
-    "E004": {"name": "Pham Thi D",  "dept": "AI",   "salary": 20000000},
-}
+diem = np.array([7.5, 8.0, 6.5, 9.0, 7.0, 8.5, 5.5, 9.5])
 
-
-print(employees["E001"]["name"])
-
-for info in employees.values():
-    if info["dept"]=="Data":
-        info["Salary"] = info["Salary"]*1.1
-print(employees)
-
+print(np.var(diem).round(2))
 
 
 
