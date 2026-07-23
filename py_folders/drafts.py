7e4e90f5c48d0312a36f51b41b3ea7c5ca6dd61e
kdkdxd <kdkdmoneyaholic@gmail.com>
2026-07-23 22:18:34 +0700

    mô tả

--- py_folders/drafts.py
+++ py_folders/drafts.py
@@ -9,33 +9,17 @@ import sys
 pd.options.display.float_format = '{:,.0f}'.format   #format :,0f
 np.random.seed(1704)
 
-don_hang = pd.DataFrame({
-    "MaDH":     [1,      2,      3,      4],
-    "MaKH":     ["KH01", "KH02", "KH01", "KH03"],
-    "SanPham":  ["Áo",   "Quần", "Giày", "Mũ"],
-    "GiaTri":   [200000, 350000, 500000, 120000]
-})
-
-khach_hang = pd.DataFrame({
-    "MaKH":    ["KH01", "KH02", "KH03", "KH04"],
-    "TenKH":   ["An",   "Bình", "Chi",  "Duy"],
-    "ThanhPho":["HCM",  "HN",   "DN",   "HCM"]
-})
-
-result_both = pd.merge(don_hang,khach_hang,on="MaKH",how="outer")
-result_inner = pd.merge(don_hang,khach_hang,on="MaKH",how="inner")
-result_left = pd.merge(don_hang,khach_hang,on="MaKH",how="left")
-result_right = pd.merge(don_hang,khach_hang,on="MaKH",how="right")
-
-print(f"\nResult_both: \n{result_both}")
-print(f"\nResult_inner: \n{result_inner}")
-print(f"\nResult_left: \n{result_left}")
-print(f"\nResult_right: \n{result_right}")
+revenueA = np.random.normal(loc=2.0, scale = 0.5, size=50)
+revenueB = np.random.normal(loc=2.3, scale = 0.5, size=50)
 
 
-
-
-
+#t-test 
+t1,pvalue1 = stats.ttest_ind(revenueA,revenueB)
+print(f"\np_value between Revenue A and Revenue B : {pvalue1:.4f}")
+if pvalue1 < 0.05:
+    print("Has Statistical Difference")
+else:
+    print("Doesn't have enough Evidence")
 
 
 
