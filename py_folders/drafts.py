46461bfc58059826199385afc3d500b8404a902b
kdkdxd <kdkdmoneyaholic@gmail.com>
2026-07-26 22:33:36 +0700

    commit

--- py_folders/drafts.py
+++ py_folders/drafts.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
+from scipy.stats import anderson
+
 import sys
 
 pd.options.display.float_format = '{:,.0f}'.format   #format :,0f
@@ -15,19 +17,11 @@ np.random.seed(1704)
 thang_x = [1,2,3,4,5,6]
 doanhthu_y = [150,200,180,250,300,280]
 
-fig,ax = plt.subplots(figsize=(8,5))
-sns.lineplot(x=thang_x,y=doanhthu_y, lw=0.7, color = "black", markers="o",mfc="red",ms=7,ax=ax)
-ax.set_title("Doanh thu theo Tháng", fontname = "Arial", fontsize = 15, fontweight="bold")
-ax.set_xlabel("Tháng", fontname = "Arial", fontsize = 10, fontweight="bold")
-ax.set_ylabel("Doanh thu (VND)", fontname = "Arial", fontsize = 10, fontweight="bold")
-plt.tight_layout()
-plt.show
-
 print("i dont know what to do now :/")
-
-
-
-
+result = anderson(doanhthu_y)
+print(f"Anderson-Darling: stat={result.statistic:.4f}")
+print(f"Critical values: {result.critical_values}")          # if Anderson-Darling < Critical values < 0.05 => Normal Distribution
+print(f"Significance levels: {result.significance_level}")   
 
 
 
